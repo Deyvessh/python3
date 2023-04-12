863908c1ccc0997ad8a6f93a2a44232b41796e53
Devesh Kumar <deveshkumar@Deveshs-MacBook-Air.local>
2023-04-13 01:09:07 +0530

    adding-bmi-and-some-leap-year-code

--- Programs/bmi-advanced.py
+++ Programs/bmi-advanced.py
@@ -6,19 +6,17 @@ weight = float(input("enter your weight in kg: "))
 bmi = weight / height ** 2
 roundoff_bmi = round(bmi, 1)
 
-print(f"Your BMI index is: {roundoff_bmi}")
-
 
 if (roundoff_bmi <= 18.4):
-    print("You are underweight")
+    print(f"Your BMI is {roundoff_bmi} and You are underweight")
 elif (roundoff_bmi <= 24.9):
-    print("You have normal weight")
+    print(f"Your BMI is {roundoff_bmi} and You have normal weight")
 elif (roundoff_bmi <= 29.9):
-    print("You are slightly overweight")
+    print(f"Your BMI is {roundoff_bmi} and You are slightly overweight")
 elif (roundoff_bmi <= 34.9):
-    print("You are obese")
+    print(f"Your BMI is {roundoff_bmi} and You are obese")
 else:
-    print("You are clinically obese")
+    print(f"Your BMI is {roundoff_bmi} and You are clinically obese")
 
 
 
---
+++ Programs/leap-year-professional2.py
@@ -0,0 +1,11 @@
+
+year = int(input("Which year do you want to check? - "))
+
+if (year % 4 == 0):
+    if (year % 100 == 0):
+        if (year % 400 == 0):
+            print(f"The year {year}is a Leap year.")
+    else:
+        print(f"The year {year} a Leap year.")
+else:
+    print(f"The year {year} not a leap year.")
