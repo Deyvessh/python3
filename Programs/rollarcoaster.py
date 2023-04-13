4593b3d6e739860f5bd154c3f97bfe8636dbcf59
Devesh Kumar <deveshkumar@Deveshs-MacBook-Air.local>
2023-04-13 21:16:29 +0530

    rollercoaster--added

--- Programs/rollarcoaster.py
+++ Programs/rollarcoaster.py
@@ -1,17 +1,33 @@
 
-height = input("Please, Enter your height in cm: ")
+int_height = int(input("Please, Enter your height in CM: "))
 
-int_height = int(height)
 
 if (int_height >= 120):
-    print("You can ride")
-    age = input("Please, Enter your age: ")
-    int_age = int(age)
+    print("Congratulations! You can ride")
+    int_age = int(input("Please, Enter your age: "))
+    
     if (int_age >= 18):
-        print("Please, get a ticket of $12 from the counter")
-    elif (int_age > 12):
-        print("Please, get a ticker of $7 from the counter")
+        photo = input("Do you want you photo clicked while riding? (Yes or No) - Extra $3 will be added to ticket: ")
+        if photo.capitalize() == 'Yes':
+            print("You final ticket for Ride + Photo is: $15")
+        else:
+            print("You final ticket for Ride is: $12")
+
+    elif (int_age >= 12 and int_age <= 17):
+        photo = input("Do you want you photo clicked while riding? (Yes or No) - Extra $3 will be added to ticket: ")
+        if photo.capitalize() == 'Yes':
+            print("You final ticket for Ride + Photo is: $10")
+        else:
+            print("You final ticket for Ride is: $7")
     else:
-        print("Please, get a ticket of $5 from the counter")        
+        photo = input("Do you want you photo clicked while riding? (Yes or No) - Extra $3 will be added to ticket: ")
+        if photo.capitalize() == 'Yes':
+            print("You final ticket for Ride + Photo is: $8")
+        else:
+            print("You final ticket for Ride is: $5")    
 else:
-    print("Sorry, you can't ride")
+    print("Sorry, you can't ride (You should be atleast 120 CM in height)")
+    
+    
+    
+    
