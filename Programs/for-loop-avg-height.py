24d8baaab36bd14e2e39b61d0b1e283c2323418d
Devesh Kumar <deveshkumar@Deveshs-MacBook-Air.local>
2023-04-22 18:14:28 +0530

    adding-files

---
+++ Programs/for-loop-avg-height.py
@@ -0,0 +1,29 @@
+
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights: ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+  
+print(student_heights)
+  
+# 156 178 165 171 187
+
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+count = 0
+
+for heightnumber in student_heights:
+    count += 1
+
+
+temp = 0 
+for sum in student_heights:
+    
+    sum += temp
+    temp = sum
+
+
+avg_height = sum / count
+print(round(avg_height))
---
+++ Programs/for-loop-with-lists.py
@@ -0,0 +1,7 @@
+
+fruits = ["Apple", "Banana", "Cherry", "Date"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+print(fruits)
